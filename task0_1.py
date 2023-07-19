9c65560513902e4b47cd2407de0e473ba73b0098
Thabani Manzini <manzini.thabani@gmail.com>
2023-07-19 11:31:06 +0200

    moved files from not-main branch

---
+++ task0_1.py
@@ -0,0 +1,13 @@
+x = 0
+y = 1
+
+if __name__ == "__main__":
+    print(x)
+    print(y)
+
+x = x + 3
+y = y + x
+
+if __name__ == "__main__":
+    print(x)
+    print(y)
---
+++ task0_4.py
@@ -0,0 +1,6 @@
+def even_or_odd(int_number):
+    print("odd") if int_number % 2 != 0 else print("even")
+
+
+if __name__ == "__main__":
+    even_or_odd(7)
